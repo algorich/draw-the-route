970c8cbd0ecbe94fcb1041f5207c70678de448bb
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2014-08-15 15:10:05 -0300

    refactoring

--- js/initialize.js
+++ js/initialize.js
@@ -18,14 +18,30 @@ function initialize() {
       zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     },
+
     nodes: [],
+
     markers: [],
+
     attachToRoad: true,
+
+    _map: undefined,
+
+    node_id: 'map_canvas',
+
+    map: function() {
+      if (this._map === undefined) {
+        this._map = new google.maps.Map(
+          document.getElementById(this.node_id), this.mapOptions
+        );
+      }
+      return this._map;
+    },
 
     newMarker: function () {
       return new google.maps.Marker({
         position: this.nodes.last(),
-        map: map
+        map: this.map
       });
     },
 
@@ -55,7 +71,7 @@ function initialize() {
         strokeColor: "#FF0000",
         strokeOpacity: 1.0,
         strokeWeight: 2,
-        map: map
+        map: this.map
       });
 
       this.updateMarkers();
@@ -112,14 +128,15 @@ function initialize() {
 
   var directionsService = new google.maps.DirectionsService();
   var directionsDisplay = new google.maps.DirectionsRenderer();
-  var map = new google.maps.Map(document.getElementById("map_canvas"), App.mapOptions);
-  directionsDisplay.setMap(map);
+  directionsDisplay.setMap(App.map());
 
-  google.maps.event.addListener(map, 'click', function(e) {
+  google.maps.event.addListener(App.map(), 'click', function(e) {
     App.addNode(e.latLng);
     App.draw();
   });
 
+
+  // map = drawTheRoute('id-do-map', {});
 
   $('#close-route').on('click', function () {
     App.closeRoute();
